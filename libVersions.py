18ec17bcb4f577c41cfd6b6b5ce7b50dff7d8bdf
Joerg Nick <nick@na.uni-tuebingen.de>
2021-03-16 15:35:37 +0100

    Expanded Readme.

---
+++ libVersions.py
@@ -0,0 +1,8 @@
+import bempp.api
+import scipy
+import numpy
+import matplotlib
+print("Bempp version :", bempp.api.__version__)
+print("Scipy version :", scipy.__version__)
+print("Numpy version :", numpy.__version__)
+print("Matplotlib version :", matplotlib.__version__)
